7bf6532efbecba0a16d4c925315b232b611065e0
George-smart <georgechinedu2018@gmail.com>
2022-06-23 22:30:09 +0100

    First Commit

---
+++ intro.js
@@ -0,0 +1,9 @@
+let firstName, lastName, height, country;
+
+firstName = "Chinedu";
+lastName = "Ekekwe";
+height = 1.75
+country = "Nigeria"
+
+document.write(`My Name is ${firstName} ${lastName}. I'm ${height}m in height. I'm from ${country}`)
+console.log(`My Name is ${firstName} ${lastName}. I'm ${height}m in height. I'm from ${country}`)
